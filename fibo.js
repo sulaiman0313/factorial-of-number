9b89aa3978a785d60763186fc4fec4d4c7601bd9
sulaiman0313 <sulemanakhtar57@gmail.com>
2024-02-16 17:35:14 +0530

    Fibonacci Sequence

---
+++ fibo.js
@@ -0,0 +1,16 @@
+
+
+
+let a=0;
+
+let b=1
+console.log(a)
+console.log(b)
+for(let i=0;i<=10;i++){
+    let fibo=a+b
+
+    console.log(fibo)
+
+    a=b;
+    b=fibo;
+}                                                  
