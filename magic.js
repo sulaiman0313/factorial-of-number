9e96c079281aa1d06deaef8a77d8516450fc739e
sulaiman0313 <sulemanakhtar57@gmail.com>
2024-02-16 16:57:50 +0530

    magicTrick

---
+++ magic.js
@@ -0,0 +1,16 @@
+
+function magic(imagineanumber){
+
+    let number= imagineanumber*2;
+
+    let number2=number+100;
+
+    let number3=number2/2;
+
+    let lastnumber= number3 - imagineanumber
+
+    return lastnumber;
+}
+let B=magic(8)
+
+console.log(B)
