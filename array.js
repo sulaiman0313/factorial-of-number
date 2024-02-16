e0dd9a56095d2827e5105820457fd2d503a027f2
sulaiman0313 <sulemanakhtar57@gmail.com>
2024-02-16 22:40:21 +0530

    find maximum value

--- array.js
+++ array.js
@@ -1,20 +1,55 @@
 
 
-function findminimum(array){
+// function findminimum(array){
 
-    let minvalue=array[0];
+//     let minvalue=array[0];
 
-    for(let i=1;i< array.length;i++){
-        if(array[i] < minvalue){
-            minvalue=array[i]
+//     for(let i=1;i< array.length;i++){
+//         if(array[i] < minvalue){
+//             minvalue=array[i]
+//         }
+//     }
+//     return minvalue
+// }
+
+// let arr=[2,1,3,4,-1,3]
+
+// let mini=findminimum(arr)
+
+// console.log(mini)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function maxvalue(array){
+    
+    let max=array[0];
+
+    for(let k=0;k<array.length;k++){
+
+        if(array[k] > max){
+            max=array[k]
         }
     }
-    return minvalue
+    return max;
 }
+const arr=[14,34,25,67]
 
-let arr=[2,1,3,4,-1,3]
+const maxi=maxvalue(arr)
+console.log(maxi)
 
-let mini=findminimum(arr)
 
-console.log(mini)
+
+
+
 
