66663314a3d252cec06021f74a385913aa76c607
sulaiman0313 <sulemanakhtar57@gmail.com>
2024-02-21 21:27:43 +0530

    object

---
+++ object.js
@@ -0,0 +1,12 @@
+
+
+let player={
+    name : "leonel Messi",
+    matches : 2300,
+    ishewonWorldcup: true,
+    goals: 900,
+    assist: 1200,
+    ballondor: 8
+}
+player.ballondor=9
+console.log(player)
